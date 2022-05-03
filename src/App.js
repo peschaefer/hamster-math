f0270ba1bdcd6fc7921d08b31cd1f9e50bb2890c
Peyton Schaefer <peschaefer@bsu.edu>
2022-05-02 21:44:09 -0400

    Initial Project Upload from Local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import animals from "./animal.js"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [age, setAge] = useState(0);
+    const [animal, setAnimal] = useState(animals[0]);
+    const [animalOldMin, setAnimalOldMin] = useState("");
+    const [animalOldMax, setAnimalOldMax] = useState("");
+    //try a useState to show the bottom label
+
+    // useEffect(() => {
+    // }, [animalOldMin]);
+
+    useEffect(() => {
+        if (animal !== null) {
+            const max = (Math.round((age / animal.maxLifeSpan) * 100) / 100).toLocaleString();
+            const min = (Math.round((age / animal.minLifeSpan) * 100) / 100).toLocaleString();
+            setAnimalOldMin(`${max}`);
+            setAnimalOldMax(`${min}`);
+        }
+    }, [age, animal]);
+
+    return (
+        <div className="App">
+            <h1 className={"title"}>Hamster Math</h1>
+            <div className={"inputRow"}>
+                <input type={"text"} className="ageBox" id={"ageBox"} placeholder={"Age Goes Here!"}/>
+                {/*<button className={"goButton"} onClick={() => {*/}
+                {/*    setAge(document.getElementById("ageBox").value);*/}
+                {/*}}>Get Results!*/}
+                {/*</button>*/}
+            </div>
+            <div className={"buttonBox"}>
+                {animals.map(animal => <button className={"animalButton"}
+                                               onClick={() => {setAnimal(animal)
+                                                   setAge(document.getElementById("ageBox").value)}}>{animal.name}</button>)}
+            </div>
+            {animalOldMin !== "0" && !isNaN(animal.maxLifeSpan) &&
+                <p className={"outputBox"}><b>You are {animalOldMin} - {animalOldMax} {animal.namePlural} old!</b></p>}
+            {isNaN(animal.maxLifeSpan) && animalOldMax !== "0" && <p className={"outputBox"}><b>You are {animalOldMax.toLocaleString()} {animal.namePlural} old!</b></p>}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/animal.js
@@ -0,0 +1,74 @@
+const animals = [
+    {
+        "name" : "hamster",
+        "namePlural" :"hamsters",
+        "minLifeSpan" : 2,
+        "maxLifeSpan" : 3
+    },
+    {
+        "name" : "elephant",
+        "namePlural" : "elephants",
+        "minLifeSpan" : 60,
+        "maxLifeSpan" : 70
+    },
+    {
+        "name" : "otter",
+        "namePlural" : "otters",
+        "minLifeSpan" : 8,
+        "maxLifeSpan" : 9
+    },
+    {
+        "name" : "tomato",
+        "namePlural" : "tomatoes",
+        "minLifeSpan" : .5,
+        "maxLifeSpan" : .67
+    },
+    {
+        "name" : "grasshopper",
+        "namePlural" : "grasshoppers",
+        "minLifeSpan" : .13,
+        "maxLifeSpan" : 1
+    },
+    {
+        "name" : "gorilla",
+        "namePlural" : "gorillas",
+        "minLifeSpan" : 35,
+        "maxLifeSpan" : 40
+    },
+    {
+        "name" : "milk",
+        "namePlural" : "milks",
+        "minLifeSpan" : .011,
+        "maxLifeSpan" : .019
+    },
+    {
+        "name" : "vine",
+        "namePlural" : "vines",
+        "minLifeSpan" : .00000022197,
+    },
+    {
+        "name" : "bat",
+        "namePlural" : "bats",
+        "minLifeSpan" : 16,
+        "maxLifeSpan" : 20
+    },
+    {
+        "name" : "goose",
+        "namePlural" : "geese",
+        "minLifeSpan" : 10,
+        "maxLifeSpan" : 24
+    },
+    {
+        "name" : "fortnite season",
+        "namePlural" : "fortnite seasons",
+        "minLifeSpan" : .219179,
+    },
+    {
+        "name" : "fortnight",
+        "namePlural" : "fortnights",
+        "minLifeSpan" : .038356,
+    }
+
+]
+
+export default animals;
